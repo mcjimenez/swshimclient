8887e659350ae1cb9cf181b74f8aab47274b7b14
Carmen Jimenez Cabezas <cjc@tid.es>
2015-04-19 21:30:56 +0200

    wip

--- js/clientConnect.js
+++ js/clientConnect.js
@@ -6,7 +6,31 @@
     console.log("CJC -*-:" + str);
   }
 
-  var _evtSectionEntries = document.getElementById('events-entries');
+  var _evtSectionEntries = document.getElementById('eventsEntries');
+  var _imgSection = document.getElementById('imgSection');
+  var _select = document.getElementById('selectIcon');
+
+
+  function changeSelect(evt) {
+    client.postMessage({ icon : _select.options[_select.selectedIndex].value });
+    debug("_select.selectedIndex:"+_select.selectedIndex);
+    debug("selected:"+_select.options[_select.selectedIndex].value);
+    _putImg({src:_select.options[_select.selectedIndex].value});
+  }
+  function _putImg(aImg) {
+    if (!aImg || !aImg.src) {
+      return;
+    }
+    var img = document.createElement('img');
+//    var imageURL = window.URL.createObjectURL(aImg.src);
+    var imageURL = aImg.src;
+    img.src = imageURL;
+    aImg.alt && img.setAttribute('alt', aImg.alt);
+    while (_imgSection.hasChildNodes()){
+      _imgSection.removeChild(_imgSection.childNodes[0]);
+    };
+    _imgSection.appendChild(img);
+  }
 
   function _addTxt(txt) {
     var li = document.createElement('li');
@@ -32,10 +56,10 @@
           _addTxt(evt.data ? JSON.stringify(evt.data): "no datas");
         };
 
-        for (var i = 0; i < NUM_MSG; i++) {
-          debug('CLIENT Send msg ' + i);
-          port.postMessage({'origin': 'client', 'secuence': i});
-        }
+        //for (var i = 0; i < NUM_MSG; i++) {
+        //  debug('CLIENT Send msg ' + i);
+        //  port.postMessage({'origin': 'client', 'secuence': i});
+        //}
     });
   };
 
@@ -48,4 +72,10 @@
   var client = new ClientConnect();
   exports.clientConnect = client;
 
+  window.addEventListener('load', function () {
+    debug('APP Document loaded! --> registrar handlers de prueba');
+    _select.addEventListener('change', changeSelect);
+    _select.selectedIndex = 0;
+    _putImg({src:_select.options[_select.selectedIndex].value});
+  });
 })(this);
