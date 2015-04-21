0fbf55874c7678061cf2d0db90f3e9ecad14f524
Carmen Jimenez Cabezas <cjc@tid.es>
2015-04-21 10:15:39 +0200

    wip

--- js/clientConnect.js
+++ js/clientConnect.js
@@ -7,37 +7,35 @@
   }
 
   var _evtSectionEntries = document.getElementById('eventsEntries');
-  var _imgSection = document.getElementById('imgSection');
-  var _select = document.getElementById('selectIcon');
-
+//  var _imgSection = document.getElementById('settingsEntries');
+  var _settingsEntries = document.getElementById('settingsEntries');
+  var _select = document.getElementById('selectSetting');
+  var whatEntry = _evtSectionEntries;
 
   function changeSelect(evt) {
-    var msg = { appicon : _select.options[_select.selectedIndex].value };
+    whatEntry = _settingsEntries;
+    var msg = { setting : _select.options[_select.selectedIndex].value };
     debug('CLIENT sending postMessage:'+ JSON.stringify(msg));
     client.postMessage(msg);
+    _removeChildren(_settingsEntries);
     debug("_select.selectedIndex:"+_select.selectedIndex);
     debug("selected:"+_select.options[_select.selectedIndex].value);
-    _putImg({src:_select.options[_select.selectedIndex].value});
-  }
-  function _putImg(aImg) {
-    if (!aImg || !aImg.src) {
-      return;
-    }
-    var img = document.createElement('img');
-//    var imageURL = window.URL.createObjectURL(aImg.src);
-    var imageURL = aImg.src;
-    img.src = imageURL;
-    aImg.alt && img.setAttribute('alt', aImg.alt);
-    while (_imgSection.hasChildNodes()){
-      _imgSection.removeChild(_imgSection.childNodes[0]);
-    };
-    _imgSection.appendChild(img);
+//    _putImg({src:_select.options[_select.selectedIndex].value});
+    //_putSetting({src:_select.options[_select.selectedIndex].value});
   }
 
-  function _addTxt(txt) {
+  function _removeChildren(aParent) {
+    debug('CLIENT removed old elements');
+    while (aParent.hasChildNodes()){
+      aParent.removeChild(_settingsEntries.childNodes[0]);
+    };
+  }
+
+  function _addTxt(txt, where) {
     var li = document.createElement('li');
     li.innerHTML = txt;
-    _evtSectionEntries.appendChild(li);
+    //_evtSectionEntries.appendChild(li);
+    where.appendChild(li);
   }
 
   var TXT_MSG = 'Testing message';
@@ -49,14 +47,16 @@
     debug('CLIENT Vamos a lanzar conexion');
     navigator.connect(URL_CONNECT).then(
       port => {
-        self.port = this.port = port;
+        self.port = port;
+        debug('CLIENT ' + JSON.stringify(this));
+        debug('CLIENT ' + JSON.stringify(port));
         //ClientConnect.port = port;
-        _addTxt("navigator.connect success. Adding listener!");
+        _addTxt("navigator.connect success. Adding listener!", _evtSectionEntries);
 
         port.onmessage = function(evt) {
           // Handle reply from the service.
           debug('CLIENT msg received --> ' + JSON.stringify(evt.data));
-          _addTxt(evt.data ? JSON.stringify(evt.data): "no datas");
+          _addTxt(evt.data ? JSON.stringify(evt.data): "no datas", whatEntry);
         };
 
         for (var i = 0; i < NUM_MSG; i++) {
